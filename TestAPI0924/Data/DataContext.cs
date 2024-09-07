5fb0d255a482eb94642b010086139f037fdf7234
Nicolas Descarpentries <nicolas.descarp@outlook.fr>
2024-09-07 16:18:45 +0200

    ajout relations entre entités dans onmodel creating

--- TestAPI0924/Data/DataContext.cs
+++ TestAPI0924/Data/DataContext.cs
@@ -13,7 +13,19 @@ namespace TestAPI0924.Data
 		public DbSet<Book> Books { get; set; }
 		public DbSet<Seller> Sellers { get; set; }
 		public DbSet<Author> Authors { get; set; }
-	
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<Book>()
+		   .HasOne(b => b.Author)
+		   .WithMany(a => a.Books)
+		   .HasForeignKey(b => b.AuthorId);
+
+
+		}
+
 
 	}
 }
