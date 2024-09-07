0d8cd3fa8b4b4099f317a677f7aee751d3430c3e
Nicolas Descarpentries <nicolas.descarp@outlook.fr>
2024-09-07 09:26:30 +0200

    add model seller

---
+++ TestAPI0924/Models/Seller.cs
@@ -0,0 +1,10 @@
+﻿namespace TestAPI0924.Models
+{
+	public class Seller
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string city { get; set; }
+	}
+}
