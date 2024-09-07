0867e5f94e6cc9858df4f974f2db743991b14efb
Nicolas Descarpentries <nicolas.descarp@outlook.fr>
2024-09-07 11:08:30 +0200

    add-relations-models

--- TestAPI0924/Models/Author.cs
+++ TestAPI0924/Models/Author.cs
@@ -1,10 +1,18 @@
-﻿namespace TestAPI0924.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAPI0924.Models
 {
 	public class Author
 	{
+		[Key]
 		public int Id { get; set; }
+
+		[Required]
 		public string FirstName { get; set; }
+
+		[Required]
 		public string LastName { get; set; } 
+		public virtual IEnumerable<Book> Books { get; set; }
 
 	}
 }
--- TestAPI0924/Models/Seller.cs
+++ TestAPI0924/Models/Seller.cs
@@ -1,10 +1,22 @@
-﻿namespace TestAPI0924.Models
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestAPI0924.Models
 {
 	public class Seller
 	{
 		public int Id { get; set; }
+
+		[Required]
 		public string FirstName { get; set; }
+
+		[Required]
 		public string LastName { get; set; }
+
+		[Required]
 		public string city { get; set; }
+
+		public virtual IEnumerable <Book> books { get; set; }	
+
 	}
 }
