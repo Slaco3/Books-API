51cb8d7f3f5dea955dad450f2355f2f2569fb426
Nicolas Descarpentries <nicolas.descarp@outlook.fr>
2024-09-08 15:57:30 +0200

    add SellerController

---
+++ TestAPI0924/Controllers/SellerController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestAPI0924.Models.DTO;
+using TestAPI0924.Models;
+using TestAPI0924.Services;
+
+namespace TestAPI0924.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class SellerController : ControllerBase
+	{
+		private readonly ISellerService _sellerService;
+
+		public SellerController(SellerService sellerService)
+		{
+			_sellerService = sellerService;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Seller>>> GetAllSellers()
+		{
+			var sellers = await _sellerService.GetAllSellersAsync();
+
+			return Ok(sellers);
+		}
+
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Seller>> GetSellerbyId(int id)
+		{
+			var seller = await _sellerService.GetSellerByIdAsync(id);
+			if (seller == null)
+			{
+				return BadRequest("Seller not found");
+			}
+			return Ok(seller);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<Seller>> AddSeller(SellerDTO sellerDTO)
+		{
+			var createdSeller = await _sellerService.AddSellerAsync(sellerDTO);	
+
+			return createdSeller;
+		}
+
+		[HttpPut("{id}")]
+		public async Task<ActionResult<Seller>> UpdateSeller(int id, SellerDTO sellerDTO)
+		{ 
+			if (id != sellerDTO.Id)
+			{
+				return BadRequest("Conflict between IDs");
+			}
+
+			var updatedSeller = await _sellerService.UpdateSellerAsync(id, sellerDTO);
+
+			if (updatedSeller == null)
+			{
+				return BadRequest("Seller doesn't exists");
+			}
+			return Ok(updatedSeller);
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<ActionResult<Seller>> DeleteSeller(int id)
+		{
+			var deletedSeller = await _sellerService.DeleteSellerAsync(id);
+			if (deletedSeller == null)
+			{
+				return BadRequest($"Unable to delete Seller {id}");
+			}
+			return Ok(deletedSeller);
+		}
+	}
+}
+}
