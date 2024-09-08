bb9b8aa5164a57e70fd68da2d1e5f01792d6a4c7
Nicolas Descarpentries <nicolas.descarp@outlook.fr>
2024-09-08 15:06:10 +0200

    ajout sellerDTO + ISellerService

---
+++ TestAPI0924/Models/DTO/SellerDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace TestAPI0924.Models.DTO
+{
+	public class SellerDTO
+	{
+		public int Id { get; set; }
+
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public string city { get; set; }
+	}
+}
---
+++ TestAPI0924/Services/ISellerService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using TestAPI0924.Models;
+using TestAPI0924.Models.DTO;
+
+namespace TestAPI0924.Services
+{
+	public interface ISellerService
+	{
+		public Task<IEnumerable<Seller>> GetAllSellersAsync();
+		public Task<Seller> GetSellerByIdAsync(int id);
+		public Task<Seller> DeleteSellerAsync(int id);
+		public Task<Seller> AddSellerAsync(SellerDTO sellerDTO);
+		public Task<Seller> UpdateSellerAsync(int id, SellerDTO sellerDTO);
+	}
+}
